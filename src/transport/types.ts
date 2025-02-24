62acb3cf7b948c5708b4261ec5c11410b7b2cf2f
Paullaster Okoth <43445963+paullaster@users.noreply.github.com>
2025-02-24 15:37:35 +0300

    adding async logger utility

--- src/transport/types.ts
+++
@@ -1,6 +0,0 @@
-export interface LoggingType {
-    url: string
-    method?: string
-    status?: number
-    message?: string
-}
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,11 @@
 import { pino } from 'pino';
-import { LoggingType } from '../transport/types.ts';
-export  const logger = pino({level: 'debug'});
-export const asyncLogging = async (options: LoggingType, type: string) => {
-    return logger.debug(options, type)
-}
+
+const d = new Date();
+const destination = process.env.APP_LOG_CHANNEL === 'daily' ? `./log-${d.getFullYear()}-0${d.getMonth()+1}-${d.getDate() < 10 ? '0'+d.getDate() : d.getDate()}`: './logfile'
+const transport = pino.transport({
+    target: '/storage/logs/pino',
+    options: {
+        destination
+    }
+})
+export  const logger = pino({level: process.env.APP_LOG_LEVEL ?? 'debug'});
