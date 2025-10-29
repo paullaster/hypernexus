0e47c240f024edd80f4ef8edfdbe0d7030e71a76
paullaster <paullaster>
2025-10-29 05:21:17 +0300

    ref: refactor the reference to the options of request option type - primaryKeys after update dating the type and pluralizing

--- src/transport/Transport.ts
+++ src/transport/Transport.ts
@@ -183,15 +183,15 @@ export class Transport {
     }
     async patch<T>(endpoint: string, payload?: any, options?: RequestOptions): Promise<T> {
         try {
-            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKey);
+            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKeys);
             const url = `${endpoint}(${primaryKeys.join(',')})`;
             if (options?.headers) {
                 options.headers['If-Match'] = "*";
             } else {
                 options = { ...options, headers: { 'If-Match': "*" } };
             }
-            if (options.primaryKey) {
-                for (const key of options.primaryKey) {
+            if (options.primaryKeys) {
+                for (const key of options.primaryKeys) {
                     delete payload[key];
                 }
             }
@@ -202,15 +202,15 @@ export class Transport {
     }
     async put<T>(endpoint: string, payload?: any, options?: RequestOptions): Promise<T> {
         try {
-            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKey);
+            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKeys);
             const url = `${endpoint}(${primaryKeys.join(',')})`;
             if (options?.headers) {
                 options.headers['If-Match'] = "*";
             } else {
                 options = { ...options, headers: { 'If-Match': "*" } };
             }
-            if (options.primaryKey) {
-                for (const key of options.primaryKey) {
+            if (options.primaryKeys) {
+                for (const key of options.primaryKeys) {
                     delete payload[key];
                 }
             }
@@ -222,7 +222,7 @@ export class Transport {
     async delete<T>(endpoint: string, payload?: any, options?: RequestOptions): Promise<T> {
         try {
             this.invalidateCache(endpoint, options);
-            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKey);
+            const primaryKeys = this.extractPrimaryKeys(payload, options?.primaryKeys);
             const url = `${endpoint}(${primaryKeys.join(',')})`;
             if (options?.headers) {
                 options.headers['If-Match'] = "*";
